4e6aa670c741edb5bd5ad3fa9790e24caaadc904
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-04-06 18:06:45 -0500

    Moved everything in to a configure block

--- app.rb
+++ app.rb
@@ -3,19 +3,21 @@ require 'sinatra/base'
 require 'sinatra/asset_pipeline'
 
 class PersonalWebsite < Sinatra::Application
-  set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg *.eot *.ttf *.woff)
-  set :assets_prefix, %w(app/assets)
-  set :assets_css_compressor, :sass
-  set :assets_js_compressor, :uglifier
+  configure do
+    set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg *.eot *.ttf *.woff)
+    set :assets_prefix, %w(app/assets)
+    set :assets_css_compressor, :sass
+    set :assets_js_compressor, :uglifier
 
-  register Sinatra::AssetPipeline
+    register Sinatra::AssetPipeline
 
-  set :views, Proc.new { File.join(root, "app/views") }
-  set :haml, layout_options: {views: 'app/views/layouts'}
+    set :views, Proc.new { File.join(root, "app/views") }
+    set :haml, layout_options: {views: 'app/views/layouts'}
 
-  if defined?(RailsAssets)
-    RailsAssets.load_paths.each do |path|
-      settings.sprockets.append_path(path)
+    if defined?(RailsAssets)
+      RailsAssets.load_paths.each do |path|
+        settings.sprockets.append_path(path)
+      end
     end
   end
 
