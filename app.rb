06fb4531c7af2f2676e4969b106f0acfc4689e1a
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-04-06 07:28:32 -0500

    Initial assets.

--- app.rb
+++ app.rb
@@ -2,11 +2,16 @@ require 'sinatra/base'
 require 'sinatra/asset_pipeline'
 
 class PersonalWebsite < Sinatra::Application
-  register Sinatra::AssetPipeline
 
   configure do
     set :haml, layout_options: {views: 'views/layouts'}
+    set :assets_precompile, %w(application.js application.css *.png *.jpg)
+    set :assets_prefix, %w(assets vendor/assets)
+    set :assets_css_compressor, :sass
+    set :assets_js_compressor, :uglifier
   end
+
+  register Sinatra::AssetPipeline
 
   get '/' do
     haml :index, layout: :application
